369b38f6edb1bbbfa466ff7b83192108a24dfe6b
Булат <4bxlia4@gmail.com>
2025-12-23 17:12:56 +0500

    Arithmetical overflow exception HANDLED

---
+++ Exceptions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int f = 1;
+            for(int i=1; i<=n; i++)
+            {
+                f *= i;
+                Console.WriteLine($"{i}! = {f}");
+            }
+        }
+    }
+}
