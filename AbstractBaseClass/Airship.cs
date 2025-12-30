76978dc3234c20e6771bbc12bce7c529db359165
Булат <4bxlia4@gmail.com>
2025-12-30 15:57:35 +0500

    DONE AbstractBaseClass

---
+++ AbstractBaseClass/Airship.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractBaseClass
+{
+    internal class Airship:AirVehicle
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Летает, имеет массивный шар");
+
+        }
+    }
+}
---
+++ AbstractBaseClass/Ship.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractBaseClass
+{
+    internal class Ship:OnWater
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Просто корабль");
+
+        }
+    }
+}
